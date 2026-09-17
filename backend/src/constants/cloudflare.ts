531950dda9f9cda01c88a3cf06b40631d0326f28
mukul0610 <mukul93028@gmail.com>
2026-09-17 18:48:00 +0530

    Update project

---
+++ backend/src/constants/cloudflare.ts
@@ -0,0 +1,6 @@
+export const CLOUDFLARE_API_PREFIX = '/api';
+export const CLOUDFLARE_HEALTH_PATH = '/health';
+export const CLOUDFLARE_ALLOWED_METHODS = 'GET,POST,PUT,PATCH,DELETE,OPTIONS';
+export const CLOUDFLARE_ALLOWED_HEADERS = 'Content-Type,Authorization,X-Request-Id';
+export const CLOUDFLARE_MAX_AGE_SECONDS = '86400';
+export const CLOUDFLARE_SERVICE_NAME = 'consulting-backend-edge';
